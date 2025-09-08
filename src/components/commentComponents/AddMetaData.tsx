f29b80fbe665bde2cbb0e519a22107eb9547f5fd
adigog0 <adigog597@gmail.com>
2025-09-08 13:04:15 +0530

    [fixes]: Metadata css property fixes

--- src/components/commentComponents/AddMetaData.tsx
+++ src/components/commentComponents/AddMetaData.tsx
@@ -19,14 +19,14 @@ const AddMetaData = ({ offsetValues, handleAddMetadata }: IProps) => {
 
   return (
     <div
-      className=" absolute gap-2 -translate-y-full min-w-[15rem]"
+      className=" absolute flex gap-2 -translate-y-full min-w-[15rem]"
       style={offsetValues ? { top: `${offsetValues.y}%`, left: `${offsetValues.x}%` } : {}}
       onClick={(e) => e.stopPropagation()}
     >
       <div className="bg-blue-400 rounded-t-2xl rounded-br-2xl text-white p-1 px-2.5 h-fit capitalize mt-auto">U</div>
 
       {/** desktop view */}
-      <div className="gap-2 bg-white p-1 rounded-xl hidden md:block">
+      <div className="gap-2 bg-white p-1 rounded-xl">
         <MetaDataInputBox handleInputValue={handleAddNewComment} input_placeholder="Add a comment" initial_value="" />
       </div>
     </div>
